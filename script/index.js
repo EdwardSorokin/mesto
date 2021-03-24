2652b63f9cfeafab43a98dd56ebb85e4c4564e23
Edward <Ed.sorokin-2014@yandex.ru>
2021-03-24 20:03:17 +0300

    mesto

---
+++ script/index.js
@@ -0,0 +1,39 @@
+let popup = document.querySelector('.popup');
+let openPopupBtn = document.getElementById('open_popup_btn');
+let closePopupBtn = document.querySelector('.popup__close');
+let PopupOverlay = document.querySelector('.popup__overlay');
+let savePopup = document.querySelector('.popup__save');
+
+function openPopup() {
+    popup.classList.add('popup_opened');
+}
+
+function closePopup() {
+    popup.classList.remove('popup_opened');
+}
+
+openPopupBtn.addEventListener('click', function (event) {
+    openPopup();
+});
+
+closePopupBtn.addEventListener('click', function (event) { closePopup(); });
+PopupOverlay.addEventListener('click', function (event) { closePopup(); });
+savePopup.addEventListener('click', function (event) { closePopup(); });
+
+let formElement = document.querySelector('.popup__form');
+let ProfileName = document.querySelector('.info__text-title');
+let ProfileText = document.querySelector('.info__text-subtitle');
+let nameInput = document.querySelector("#nameInput");
+let jobInput = document.querySelector("#jobInput");
+
+function formSubmitHandler(evt) {
+    evt.preventDefault();
+
+    ProfileName.textContent = nameInput.value;
+    ProfileText.textContent = jobInput.value;
+}
+
+formElement.addEventListener('submit', formSubmitHandler);
+
+
+
