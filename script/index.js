eed5d3e0ea0f460c8341e46ce497009c4ed27fb2
Edward <Ed.sorokin-2014@yandex.ru>
2021-03-26 03:27:23 +0300

    main-mesto-10

--- script/index.js
+++ script/index.js
@@ -1,38 +1,34 @@
 let popup = document.querySelector('.popup');
 let openPopupBtn = document.getElementById('open_popup_btn');
 let closePopupBtn = document.querySelector('.popup__close');
-let PopupOverlay = document.querySelector('.popup__overlay');
+let popupOverlay = document.querySelector('.popup__overlay');
 let savePopup = document.querySelector('.popup__form-save');
+let formElement = document.querySelector('.popup__form');
+let profileName = document.querySelector('.info__text-title');
+let profileText = document.querySelector('.info__text-subtitle');
+let nameInput = document.querySelector("#nameInput");
+let jobInput = document.querySelector("#jobInput");
 
 function openPopup() {
     popup.classList.add('popup_opened');
+    nameInput.value = profileName.textContent;
+    jobInput.value = profileText.textContent;
 }
 
 function closePopup() {
     popup.classList.remove('popup_opened');
 }
 
-openPopupBtn.addEventListener('click', function (event) {
-    openPopup();
-});
-
-closePopupBtn.addEventListener('click', function (event) { closePopup(); });
-PopupOverlay.addEventListener('click', function (event) { closePopup(); });
-savePopup.addEventListener('click', function (event) { closePopup(); });
-
-let formElement = document.querySelector('.popup__form');
-let ProfileName = document.querySelector('.info__text-title');
-let ProfileText = document.querySelector('.info__text-subtitle');
-let nameInput = document.querySelector("#nameInput");
-let jobInput = document.querySelector("#jobInput");
-
 function formSubmitHandler(evt) {
     evt.preventDefault();
-
-    ProfileName.textContent = nameInput.value;
-    ProfileText.textContent = jobInput.value;
+    profileName.textContent = nameInput.value;
+    profileText.textContent = jobInput.value;
+    closePopup();
 }
 
+openPopupBtn.addEventListener('click', function (event) { openPopup(); });
+closePopupBtn.addEventListener('click', function (event) { closePopup(); });
+popupOverlay.addEventListener('click', function (event) { closePopup(); });
 formElement.addEventListener('submit', formSubmitHandler);
 
 
